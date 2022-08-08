3361b38359eeb19bd4eb0c272f2a05bdebb09692
sam <sam@sams-MacBook-Air.local>
2022-08-09 00:15:14 +0430

    change icon and app name

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Hello world!!.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
