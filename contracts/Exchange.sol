14b993c59eea36566ca7cf3ff31e7e75f71fa4d5
sam <sam@sams-MacBook-Air.local>
2022-11-07 01:50:43 +0330

    add depositToken function

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -2,12 +2,22 @@
 pragma solidity ^0.8.9;
 
 import "hardhat/console.sol";
+import "./Ding.sol";
 
 contract Exchange {
     address public feeAccount;
     uint256 public feePercentage;
+
+    mapping(address => mapping(address => uint256)) public balanceOf;
+
     constructor (address _feeAccount, uint256 _feePercentage){
         feeAccount = _feeAccount;
         feePercentage = _feePercentage;
+    }
+
+    function depositToken(address _token, uint256 _value) public  returns (bool success){
+        require(Ding(_token).transferFrom(msg.sender,address(this), _value));
+        balanceOf[msg.sender][_token] = balanceOf[msg.sender][_token] + _value;
+        return true;
     }
 }
--- test/Exchange.ts
+++ test/Exchange.ts
@@ -1,5 +1,4 @@
 import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 function tokens(n: number){
@@ -7,12 +6,17 @@ function tokens(n: number){
 }
 describe("Exchange", ()=>{
     async function deployExchange() {
-        const [owner, otherAccount, feeAccount] = (await ethers.getSigners());
+        const [owner, user1, user2, feeAccount] = (await ethers.getSigners());
         const feePercentage = 2;
-        const Exchange = await ethers.getContractFactory("Exchange")
+        const Exchange = await ethers.getContractFactory("Exchange");
         const exchange = await Exchange.deploy(feeAccount.address, feePercentage);
-        
-        return {exchange, Exchange, owner, otherAccount, feeAccount, feePercentage}
+        const Ding = await ethers.getContractFactory("Ding");
+        const ding = await Ding.deploy("Ding Token", "DNG", "0.1.0", 1000000)
+        const transaction = await ding.connect(owner).transfer(user1.address,tokens(100))
+        await transaction.wait();
+        const transaction2 = await ding.connect(owner).transfer(user2.address,tokens(100))
+        await transaction2.wait();
+        return {exchange, Exchange, owner, user1, user2, feeAccount, feePercentage, Ding, ding}
     }
     
     describe("Deployment",()=>{
@@ -28,6 +32,24 @@ describe("Exchange", ()=>{
         });
 
     });
-
+    describe("Deposit token",()=>{
+        describe("Happy scenario",()=>{
+            it("deposit token successfully", async()=>{
+                const{ding, exchange, user1} = await loadFixture(deployExchange);
+                const transaction1 = await ding.connect(user1).approve(exchange.address, tokens(10));
+                await transaction1.wait();
+                const transaction2 = await exchange.connect(user1).depositToken(ding.address, tokens(10));
+                await transaction2.wait();
+                expect(await ding.balanceOf(exchange.address)).to.equals(tokens(10));
+                expect(await exchange.balanceOf(user1.address,ding.address)).to.equals(tokens(10));
+            })
+        })
+        describe("Sad scenario", ()=>{
+            it("don't have allowance to deposit",async()=> {
+                const{ding, exchange, user1} = await loadFixture(deployExchange);
+                await expect(exchange.connect(user1).depositToken(ding.address, tokens(10))).to.be.reverted;
+            })
+        })
+    })
 
 })
