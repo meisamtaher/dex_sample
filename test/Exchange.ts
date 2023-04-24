8125266418f2c2ac3b085d988779985decb0deac
sam <sam@sams-MacBook-Air.local>
2023-04-24 04:33:15 +0330

    add withdraw tests

--- test/Exchange.ts
+++ test/Exchange.ts
@@ -43,11 +43,58 @@ describe("Exchange", ()=>{
                 expect(await ding.balanceOf(exchange.address)).to.equals(tokens(10));
                 expect(await exchange.balanceOf(user1.address,ding.address)).to.equals(tokens(10));
             })
+            it("emit deposit event", async()=>{
+                const{ding, exchange, user1} = await loadFixture(deployExchange);
+                const amount = 10;
+                const transaction1 = await ding.connect(user1).approve(exchange.address, tokens(amount));
+                await transaction1.wait();
+                await expect(exchange.connect(user1).depositToken(ding.address, tokens(amount)))
+                .to.emit(exchange,"Deposit")
+                .withArgs(ding.address, user1.address, tokens(amount), tokens(amount));
+            })
+            
         })
         describe("Sad scenario", ()=>{
             it("don't have allowance to deposit",async()=> {
                 const{ding, exchange, user1} = await loadFixture(deployExchange);
                 await expect(exchange.connect(user1).depositToken(ding.address, tokens(10))).to.be.reverted;
+            })
+        })
+    })
+    describe("Withdraw token",()=>{
+        describe("Happy scenario",()=>{
+            it("withdraw token successfully", async()=>{
+                const{ding, exchange, user1} = await loadFixture(deployExchange);
+                var transaction = await ding.connect(user1).approve(exchange.address, tokens(10));
+                await transaction.wait();
+                transaction = await exchange.connect(user1).depositToken(ding.address, tokens(10));
+                await transaction.wait();
+                transaction = await exchange.connect(user1).withdrawToken(ding.address, tokens(10));
+                await transaction.wait();
+                expect(await ding.balanceOf(exchange.address)).to.equals(0);
+                expect(await exchange.balanceOf(user1.address,ding.address)).to.equals(0);
+                expect(await ding.balanceOf(user1.address)).to.equals(tokens(100));
+            })
+            it("emit withdraw event", async()=>{
+                const{ding, exchange, user1} = await loadFixture(deployExchange);
+                var transaction = await ding.connect(user1).approve(exchange.address, tokens(10));
+                await transaction.wait();
+                transaction = await exchange.connect(user1).depositToken(ding.address, tokens(10));
+                await transaction.wait();
+                await expect(exchange.connect(user1).withdrawToken(ding.address, tokens(10)))
+                .to.emit(exchange, "Withdraw")
+                .withArgs(ding.address, user1.address, tokens(10), 0)
+            })
+            
+        })
+        describe("Sad scenario", ()=>{
+            it("don't have balance to withdraw",async()=> {
+                const{ding, exchange, user1, user2} = await loadFixture(deployExchange);
+                var transaction = await ding.connect(user1).approve(exchange.address, tokens(10));
+                await transaction.wait();
+                transaction = await exchange.connect(user1).depositToken(ding.address, tokens(10));
+                await transaction.wait();
+                await expect(exchange.connect(user2).withdrawToken(ding.address, tokens(10))).to.be.reverted;
             })
         })
     })
